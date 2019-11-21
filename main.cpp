991af0fa990bf46d0a7b1f55dc1a48883bb2504d
Gerson03 <54824379+Gerson03@users.noreply.github.com>
2019-11-21 20:52:42 +0800

    Add files via upload
    Class activity

--- main.cpp
+++ main.cpp
@@ -1,100 +1,78 @@
 #include <iostream>
-#include <string>
 #include <stdlib.h>
 using namespace std;
 
-void title();
+class Product
+{
+public:
+    void addproduct(string ID, string prodname, string exp, string manu, int price);
+    double display(void);
+    string answer;
 
-void GetInfo(string [], string [], string [], string [], int [], int[]);
-void display(string [], string [], string [], string [], int [], int[]);
+    Product();
 
-int arraysize = 10;
+private:
+    string ID, prodname, exp, manu;
+    int price;
+
+};
+
+Product :: Product(void)
+{
+    cout << "\t\t+++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout << "\t\t+          W E L C O M E  U S E R           +"<<endl;
+    cout << "\t\t+++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout <<endl;
+
+   do {
+        {
+
+
+    cout << "\t\tEnter product ID : ";
+    cin >>ID;
+
+    cout << "\t\tEnter product name : ";
+    cin >>prodname;
+
+    cout << "\t\tEnter product expiration date : ";
+    cin >>exp;
+
+    cout << "\t\tEnter product manufacturer : ";
+    cin >>manu;
+
+    cout << "\t\tEnter product price : ";
+    cin >>price;
+    cout <<endl;
+        }
+        cout << "\t\tDo you want to add product ?"<<endl;
+        cin >>answer;
+   }
+        while (answer == "Y" || answer == "y");
+}
+
+double Product::display(void)
+{
+    cout << endl;
+    cout << "\t\t+++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout << "\t\t+   P R O D U C T  I N F O R M A T I O N    +"<<endl;
+    cout << "\t\t+++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout <<endl;
+    cout << "\t\tEnter product ID : "<<ID<<endl;;
+    cout << "\t\tEnter product name : "<<prodname<<endl;
+    cout << "\t\tEnter product expiration date : "<<exp<<endl;
+    cout << "\t\tEnter product manufacturer : "<<manu<<endl;
+    cout << "\t\tEnter product price : "<<price<<endl;
+    cout <<endl;
+    cout << "\t\t+++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+
+}
 
 int main()
 {
+    Product prod;
 
-    string lname[10];
-    string fname[10];
-    string email[10];
-    string address[10];
-    int age[10];
-    int contact[10];
-
-
-    GetInfo(lname, fname, email, address, age, contact);
-    display(lname, fname, email, address, age, contact);
-
-
-
+    prod.display();
     return 0;
-}
-
-
-
-
-
-
-void GetInfo(string lname [], string fname[], string email[], string address[], int age [], int contact[])
-{
-
-
-    cout << "Enter your last name"<<endl;
-    for (int i = 0; i <= arraysize -1; i++)
-    {
-        cout << "Last name : " << i +1 << ": ";
-        cin >> lname [i];
-    }
-system ("CLS");
-    cout << "Enter your first name"<<endl;
-    for (int i = 0; i <= arraysize -1; i++)
-    {
-        cout << "First name : " << i +1 << ": ";
-        cin >> fname [i];
-    }
-system ("CLS");
-    cout << "Enter your address"<<endl;
-    for (int i = 0; i <= arraysize -1; i++)
-    {
-        cout << "Address : " << i +1 << ": ";
-        cin >> address [i];
-    }
-system ("CLS");
-    cout << "Enter your email address"<<endl;
-    for (int i = 0; i <= arraysize -1; i++)
-    {
-        cout << "Email : " << i +1 << ": ";
-        cin >> email [i];
-    }
-system ("CLS");
-    cout << "Enter your age"<<endl;
-     for (int i = 0; i <= arraysize -1; i++)
-    {
-        cout << "Age : " << i +1 << ": ";
-        cin >> age [i];
-    }
-system ("CLS");
-    cout << "Enter your contact number3"<<endl;
-     for (int i = 0; i <= arraysize -1; i++)
-    {
-        cout << "Contact : " << i +1 << ": ";
-        cin >> email [i];
-    }
-
-
-}
-
-
-void display(string lname[], string fname[], string email[], string address[], int age[], int contact[])
-{
-system ("CLS");
-cout << "Last name|First name|Email|Address|Age|Contact number"<<endl;
-    for (int i = 0; i <= arraysize -1; i++)
-    {
-
-        cout << lname [i] << "  " << fname [i]<< "  " <<email [i]<< "  " <<address [i] << "  " <<age [i] << "  " <<contact [i]<<endl;
-
-    }
-
 }
 
 
